f91fcd80a7fe823fba3d861fd843d304e2b61cea
kpretzer <kpretzer@calpoly.edu>
2014-11-11 21:25:27 -0800

    Addition
    Added process statement and incorporated in preprocessing, processing
    message in first 512-bit chunk, and initializing working variables.

--- src/hash_algorithm.vhd
+++ src/hash_algorithm.vhd
@@ -14,7 +14,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -32,7 +32,8 @@ begin
 	--Initialize hash values: The hash values are initialized to the first 
 		--32 bits of the fractional parts of the square roots of the first 8 
 		--prime numbers (2..19).
-	constant h_original is array (0 to 7) of STD_LOGIC_VECTOR(31 downto 0) := 
+	type vector_array_16by32 is array (0 to 15) of STD_LOGIC_VECTOR(31 downto 0);
+	constant h_original : vector_array_16by32 := 
 		(x"6a09e667", x"bb67ae85", x"3c6ef372", x"a54ff53a", x"510e527f",
 		 x"9b05688c", x"1f83d9ab", x"5be0cd19");
 	
@@ -51,22 +52,67 @@ begin
 		 x"5b9cca4f", x"682e6ff3", x"748f82ee", x"78a5636f", x"84c87814", x"8cc70208",
 		 x"90befffa", x"a4506ceb", x"bef9a3f7", x"c67178f2");
 
+
+	--This process statement contains the workings of the hash algorithm. The inputs include 
+		--the original message array and the arays of constants defined above. The output is 
+		--the final processed hash value.
+	algorithm: process(message)
+	begin
+	
+	--General Variable Declarations (See working variable declaration below)
+			variable index1 : integer := 0;
+			variable w is array (0 to 63) of STD_LOGIC_VECTOR(31 downto 0);
+			variable temp_message is array (0 to 63) of STD_LOGIC_VECTOR(31 downto 0);
+			variable s0: std_logic := 0;
+			variable s1: std_logic := 0;
+
+	--Preprocessing: Append the bit '1' to message.
+		
+			--Initialize message array and pad with zeros.
+			init_temp_message: for index1 in 0 to 63 loop
+				temp_message := x"00000000";
+			end loop init_temp_message;
+			
+			temp_message <= message & x"00000001";
+				
+	--Process Message in Successive 512-bit Chuncks
+		
+			--Initialize message array.
+			init_w: for index1 in 0 to 63 loop
+				w := x"00000000";
+			end loop init_w;
+			
+			--Copy chunk into first 16 words of message schedule array.
+			copy_first16: for index1 in 0 to 15 loop
+				w(i) := temp_message(i);
+			end loop copy_first16;
+			
+			--Extend first 16 words into remaining 48 words of message schedule array.
+			extend_next48: for index1 in 16 to 63 loop
+				s0   := (w(i-15) ror 7) XOR (w(i-15) ror 18) XOR (w(i-15) sra 3);
+				s1   := (w(i-2) ror 17) XOR (w(i-2) ror 19) XOR (w(i-2) sra 10);
+				w(i) := w(i-16) + s0 + w(i-7) + s1;
+			end loop extend_next48;
+			
+	--Initialize Working Variables to Current Hash Value
+			variable a : vector_array_16by32 := h_original(0);
+			variable b : vector_array_16by32 := h_original(1);
+			variable c : vector_array_16by32 := h_original(2);
+			variable d : vector_array_16by32 := h_original(3);
+			variable e : vector_array_16by32 := h_original(4);
+			variable f : vector_array_16by32 := h_original(5);
+			variable g : vector_array_16by32 := h_original(6);
+			variable h : vector_array_16by32 := h_original(7);
+
+	--Compression Function Main Loop
+
+
+	--Add Compressed Chunk to Current Hash Value
+
+
+	--Produce Final Hash Value (Big-Endian)
+
+
+	end process algorithm;
+	
 end Behavioral;
-
-
---Preprocessing: Append the bit '1' to message.
-	
-	
---Process Message in Successive 512-bit Chuncks
-	
-	
---Initialize Working Variables to Current Hash Value
-
-
---Compression Function Main Loop
-
-
---Add Compressed Chunk to Current Hash Value
-
-
---Produce Final Hash Value (Big-Endian)
